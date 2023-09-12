d1888b46852daadb5be626e02a72f568b2e9703b
najmul-islam <itzmenajmul@gmail.com>
2023-09-13 03:50:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="md:flex">
+        <Blogs />
+        <Bookmarks/>
+      </div>
+    </>
+  );
+}
+
+export default App;
